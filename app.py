b53f9e15c541e4e57064dfe5321e7056aecd6507
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-09-26 00:40:24 +0400

    almost there

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import secret
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome("./chromedriver")  # Optional argument, if not specified will search path.
 driver.get('https://nyuad.dserec.com/online/capacity/');
@@ -17,6 +20,25 @@ pwd = driver.find_element_by_xpath("//*[@id=\"password\"]")
 pwd.send_keys(secret.password)
 confirm_sso = driver.find_element_by_xpath("//*[@id=\"login\"]/button").click()
 
-driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+WebDriverWait(driver, 50).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,'iframe')))
 
 push = driver.find_element_by_xpath("//*[@id=\"auth_methods\"]/fieldset/div[1]/button").click()
+
+WebDriverWait(driver, 50).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,'iframe')))
+
+driver.implicitly_wait(20)
+
+fitness = driver.find_element_by_xpath("//*[@id=\"page\"]/div/section/div/div/div/div/div/div/div/div/div[1]/div[1]/ul/li[7]/a").click()
+
+driver.find_element_by_xpath("//*[@id=\"page\"]/div/section/div/div/div/div/div/div/div/div/div[1]/div[2]/div[3]/a[2]").click()
+
+driver.find_element_by_xpath("//*[@id=\"page\"]/div/section/div/div/div/div/div/div/div/div/div[1]/div[2]/div[3]/a[2]").click()
+
+# selecting time 
+# has to be different for some days 
+driver.find_element_by_xpath("//*[@id=\"page\"]/div/section/div/div/div/div/div/div/div/div/div[1]/div[2]/div[4]/div/div/div/div/div[3]").click()
+
+driver.implicitly_wait(3)
+
+# clicking reserve
+driver.find_element_by_xpath("//*[@id=\"page\"]/div/section/div/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[2]/button[2]").click()
